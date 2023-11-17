4b0587074fae9e134543cfec5b0adbee35dd6d26
gds90 <you@example.com>
2023-11-17 13:04:19 +0100

    Esercizio 3

--- js/script.js
+++ js/script.js
@@ -9,3 +9,14 @@ console.log(PI);
 // esercizio 2/B
 // PI = 3.1416;
 // console.log(PI);
+
+// esercizio 2/C
+let radius = 8;
+let circle = radius * 2 * PI
+console.log(circle)
+
+// esercizio 3
+let name = 'Carlo'
+console.log(name)
+name = 'Marco'
+console.log(name)
